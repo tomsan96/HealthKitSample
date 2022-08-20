1cdfbf80ab944da09a9c4e4215eb525c521fa880
tomsan96 <gradada0507@gmail.com>
2022-08-21 04:21:22 +0900

    1週間の歩数表示

--- HealthKitSample/ContentView.swift
+++ HealthKitSample/ContentView.swift
@@ -21,26 +21,13 @@ struct ContentView: View {
         
         // 歩数データを取得する関数を呼ぶ（引数は期間）
         contentVM.get(
-            fromDate: dateformatter.date(from: "2022/01/01 00:00:00")!,
-            toDate: dateformatter.date(from: "2022/03/01 23:59:59")!
+            fromDate: dateformatter.date(from: "2022/06/15 00:00:00")!,
+            toDate: dateformatter.date(from: "2022/06/21 23:59:59")!
         )
     }
     
     var body: some View {
-        NavigationView {
-            List {
-                if contentVM.dataSource.count == 0 {
-                    Text("データがありません。")
-                } else {
-                    ForEach( contentVM.dataSource ){ item in
-                        HStack{
-                            Text(dateformatter.string(from: item.datetime))
-                            Text(" \(item.count)")
-                        }
-                    }
-                }
-            }.navigationBarTitle(Text("歩数一覧"), displayMode: .inline)
-        }
+        Text(String(contentVM.totalCount))
     }
 }
 
@@ -59,6 +46,7 @@ struct ListRowItem: Identifiable {
 class ContentViewModel: ObservableObject, Identifiable {
  
     @Published var dataSource:[ListRowItem] = []
+    @Published var totalCount: Double = 0.0
     
     func get( fromDate: Date, toDate: Date)  {
  
@@ -79,30 +67,28 @@ class ContentViewModel: ObservableObject, Identifiable {
                 return
             }
             
-            // 歩数を取得
-            let query = HKSampleQuery(sampleType: HKSampleType.quantityType(forIdentifier: .stepCount)!,
-                                           predicate: HKQuery.predicateForSamples(withStart: fromDate, end: toDate, options: []),
-                                           limit: HKObjectQueryNoLimit,
-                                           sortDescriptors: [NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: true)]){ (query, results, error) in
-                
-                guard error == nil else { print("error"); return }
-                
-                if let tmpResults = results as? [HKQuantitySample] {
-                    
-                    // 取得したデータを１件ずつ ListRowItem 構造体に格納
-                    // ListRowItemは、dataSource配列に追加します。ViewのListでは、この dataSource配列を参照して歩数を表示します。
-                    for item in tmpResults {
- 
-                        let listItem = ListRowItem(
-                            id: item.uuid.uuidString,
-                            datetime: item.endDate,
-                            count: String(item.quantity.doubleValue(for: .count()))
-                        )
- 
-                        self.dataSource.append(listItem)
+            let distanceType = HKObjectType.quantityType(forIdentifier: .stepCount)!
+            let predicate = HKQuery.predicateForSamples(withStart: fromDate, end: toDate, options: [])
+            
+            let query = HKStatisticsQuery(
+                quantityType: distanceType,
+                quantitySamplePredicate: predicate,
+                options: [.cumulativeSum]) { query, statistics, error in
+                    print(statistics!.sumQuantity()!) // 6902 count
+                    if let quantity = statistics?.sumQuantity() {
+                        let stepValue = quantity.doubleValue(for: HKUnit.count())
+                        
+                        DispatchQueue.main.async {
+                            print(stepValue)
+                            self.totalCount = stepValue
+                        }
+                        
+                    } else {
+                        DispatchQueue.main.async {
+                            print("0歩数")
+                        }
                     }
                 }
-            }
             healthStore.execute(query)
         })
     }
