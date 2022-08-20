e3d3fdedae228863a3a89a561f492caa5eac438e
tomsan96 <gradada0507@gmail.com>
2022-08-20 16:26:53 +0900

    first commit

---
+++ HealthKitSample/HealthKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitSampleApp.swift
+//  HealthKitSample
+//
+//  Created by 山崎定知 on 2022/08/20.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
